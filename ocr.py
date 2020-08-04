ed2e4c052fa6579b69f35b5d0b7b41c3e00d07a3
YogeshUpadhyay <yogeshh2021@gmail.com>
2020-08-04 16:11:27 +0530

    Implemented on the test file

--- ocr.py
+++ ocr.py
@@ -1,8 +1,9 @@
 import easyocr
 import torch
 
+
 reader = easyocr.Reader(['en']) # need to run only once to load model into memory
 
-text = reader.readtext('IDCard.jpg')
+text = reader.readtext('Challan.jpeg',detail=0)
 
 print(text)
