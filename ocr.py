c84b1d526bf36dff7f43344b3198739c9037d434
YogeshUpadhyay <yogeshh2021@gmail.com>
2020-08-23 18:52:28 +0530

    annotations compplete

--- ocr.py
+++ ocr.py
@@ -1,9 +1,16 @@
 import easyocr
 import torch
+import cv2
+import PIL as pil
+import matplotlib.pyplot as plt
 
+#reader = easyocr.Reader(['en']) # need to run only once to load model into memory
 
-reader = easyocr.Reader(['en']) # need to run only once to load model into memory
+#text = reader.readtext('Challan.jpeg',detail=0)
 
-text = reader.readtext('Challan.jpeg',detail=0)
+#print(text)
 
-print(text)
+im = plt.imread('Challan.jpeg')
+
+plt.imshow(im)
+plt.show()
