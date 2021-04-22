fcf9f8baa967045dfb3512dad144d4d88798d8b8
Yogesh Upadhyay <yogeshh2021@gmail.com>
2021-04-22 15:38:26 +0530

    data processing added

--- src/dataprocessing.py
+++ src/dataprocessing.py
@@ -1,3 +1,17 @@
+from .imageprocessing import ImageProcessor
+from .textprocessing import TextProcessor
+
 class DataProcessing:
     def __init__(self):
         pass
+
+    def process_data(self):
+        # detect cells from the image
+        imageprocessor = ImageProcessor()
+        img_cells = imageprocessor.get_cells()
+
+        # detect text from the img_cells
+        textprocessor = TextProcessor()
+        text_cells = textprocessor.get_cells()
+
+        return text_cells
